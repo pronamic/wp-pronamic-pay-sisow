45ea507451bae7c50d4410c86155ec3084f5cdb3
Remco Tolsma <info@remcotolsma.nl>
2014-09-09 11:04:31 +0200

    Moved class from the Pronamic iDEAL plugin to this library.

---
+++ src/Config.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Title: Sisow config
+ * Description:
+ * Copyright: Copyright (c) 2005 - 2014
+ * Company: Pronamic
+ * @author Remco Tolsma
+ * @version 1.0.0
+ */
+class Pronamic_WP_Pay_Gateways_Sisow_Config extends Pronamic_WP_Pay_GatewayConfig {
+	public $merchant_id;
+
+	public $merchant_key;
+}
