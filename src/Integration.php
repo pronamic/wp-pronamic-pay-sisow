70e2a47e101be0477932fa74658ddfca55c2326d
Remco Tolsma <info@remcotolsma.nl>
2015-11-12 14:00:38 +0100

    Simplified integration class.

---
+++ src/Integration.php
@@ -0,0 +1,27 @@
+<?php
+
+class Pronamic_WP_Pay_Gateways_SisowIntegration {
+	public function __construct() {
+		$this->id            = 'sisow-ideal';
+		$this->name          = 'Sisow';
+		$this->url           = 'https://www.sisow.nl/';
+		$this->dashboard_url = 'https://www.sisow.nl/';
+		$this->provider      = 'sisow';
+	}
+
+	public function get_config_factory_class() {
+		return 'Pronamic_WP_Pay_Gateways_Sisow_ConfigFactory';
+	}
+
+	public function get_config_class() {
+		return 'Pronamic_WP_Pay_Gateways_Sisow_Config';
+	}
+
+	public function get_settings_class() {
+		return 'Pronamic_WP_Pay_Gateways_Sisow_Settings';
+	}
+
+	public function get_gateway_class() {
+		return 'Pronamic_WP_Pay_Gateways_Sisow_Gateway';
+	}
+}
