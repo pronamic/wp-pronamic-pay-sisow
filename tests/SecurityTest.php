9c6f8ffdb4983da177757c2acaa64f74ca4e25d7
Remco Tolsma <info@remcotolsma.nl>
2016-02-05 10:23:56 +0100

    Added documentation block.

--- tests/SecurityTest.php
+++ tests/SecurityTest.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * Title: Sisow security test
+ * Description:
+ * Copyright: Copyright (c) 2005 - 2016
+ * Company: Pronamic
+ *
+ * @author Remco Tolsma
+ * @version 1.0.0
+ */
 class Pronamic_WP_Pay_Gateways_Sisow_SecurityTest extends WP_UnitTestCase {
 	function test_transaction_request_sha1() {
 		// http://pronamic.nl/wp-content/uploads/2013/02/sisow-rest-api-v3.2.1.pdf #page 10
