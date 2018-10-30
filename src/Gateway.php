58e6db397f7011f352096dec38e64babcd7e8b25
Reüel van der Steege <reuel@pronamic.nl>
2018-10-30 16:15:07 +0100

    Fix setting incorrect birth date parameter.

--- src/Gateway.php
+++ src/Gateway.php
@@ -174,7 +174,7 @@ class Gateway extends Core_Gateway {
 			}
 
 			if ( null !== $customer->get_birth_date() ) {
-				$request->set_parameter( 'birth_date', $customer->get_birth_date()->format( 'dmY' ) );
+				$request->set_parameter( 'birthdate', $customer->get_birth_date()->format( 'dmY' ) );
 			}
 		}
 
