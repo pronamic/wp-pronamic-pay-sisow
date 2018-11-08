34673be6e908da3904b21f8d7725519aa3ffb2aa
Remco Tolsma <info@remcotolsma.nl>
2018-11-08 20:05:07 +0100

    Added Capayable.

--- src/Gateway.php
+++ src/Gateway.php
@@ -87,6 +87,7 @@ class Gateway extends Core_Gateway {
 			PaymentMethods::BELFIUS,
 			PaymentMethods::BILLINK,
 			PaymentMethods::BUNQ,
+			PaymentMethods::CAPAYABLE,
 			PaymentMethods::IN3,
 			PaymentMethods::CREDIT_CARD,
 			PaymentMethods::FOCUM,
