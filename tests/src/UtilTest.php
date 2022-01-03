51ecaf9eb50439e0905a120f82af7f8049f6e698
Reüel van der Steege <reuel@pronamic.nl>
2022-01-03 14:05:55 +0100

    Happy 2022.

--- tests/src/UtilTest.php
+++ tests/src/UtilTest.php
@@ -3,7 +3,7 @@
  * Util test.
  *
  * @author    Pronamic <info@pronamic.eu>
- * @copyright 2005-2021 Pronamic
+ * @copyright 2005-2022 Pronamic
  * @license   GPL-3.0-or-later
  * @package   Pronamic\WordPress\Pay\Gateways\Sisow
  */
@@ -15,7 +15,7 @@ use WP_UnitTestCase;
 /**
  * Title: Sisow util test
  * Description:
- * Copyright: 2005-2021 Pronamic
+ * Copyright: 2005-2022 Pronamic
  * Company: Pronamic
  *
  * @author  Remco Tolsma
